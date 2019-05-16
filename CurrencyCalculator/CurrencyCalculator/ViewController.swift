5a42247801e2a83959fcd958abda56237f56fd20
giuk.jung <asahijung@gmail.com>
2019-05-16 12:09:29 +0900

    [add]Create single view project

---
+++ CurrencyCalculator/CurrencyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyCalculator
+//
+//  Created by iMac27ch on 2019/05/16.
+//  Copyright © 2019年 giuk.jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
